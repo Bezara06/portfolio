13ff00c8e9b42828e4f6f6ca9b09838b0e2a58cd
Bezara06 <anicebezara@gmail.com>
2025-06-04 08:57:29 +0200

    just little thing

---
+++ php/db.php
@@ -0,0 +1,7 @@
+<?php
+$db = "client";
+$host = "localhost";
+$password = "";
+$user = "pma";
+
+$con = mysqli_connect($host,$user,$password,$db);
---
+++ php/login.php
@@ -0,0 +1,23 @@
+<?php
+
+include('db.php');
+
+if(isset($_POST['LogIn'])){
+    $uName = $_POST['nom'];
+    $uPass = $_POST['sequirity'];
+
+    $stat = mysqli_query($con, "SELECT * FROM USERS WHERE `password` LIKE('$uPass') AND nom LIKE('$uName') OR `password` LIKE('$uPass') AND email LIKE('$uName')");
+
+    if($stat->num_rows == 0){
+        header("Location: ../index.html");
+        #echo "alert('Connectée!')";
+    }else{
+        while ($result = mysqli_fetch_assoc($stat)) {
+            # code...
+            header("Location: ../html/about.html");
+        }
+    }
+    
+}else{
+    header("Location: ../index.html");
+}
